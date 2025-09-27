30338dc1cedf033379e6ed3bd5f6c2a5faf24e41
Spike271 <sabnanimayank091@gmail.com>
2025-09-27 16:29:37 +0530

    first commit

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,412 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Point = New Button()
+        EqualBtn = New Button()
+        PlusBtn = New Button()
+        MinusBtn = New Button()
+        MulBtn = New Button()
+        DivBtn = New Button()
+        Back = New Button()
+        Clear = New Button()
+        Clear_All = New Button()
+        expression = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button1.AutoSize = True
+        Button1.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button1.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button1.Location = New Point(114, 407)
+        Button1.MinimumSize = New Size(64, 56)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(64, 56)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBox1.BackColor = Color.FromArgb(CByte(64), CByte(0), CByte(64))
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Font = New Font("A-OTF Jun Pro 34", 26.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        TextBox1.ForeColor = Color.White
+        TextBox1.Location = New Point(112, 51)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "0"
+        TextBox1.ReadOnly = True
+        TextBox1.RightToLeft = RightToLeft.No
+        TextBox1.Size = New Size(464, 77)
+        TextBox1.TabIndex = 1
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        TextBox1.WordWrap = False
+        ' 
+        ' Button2
+        ' 
+        Button2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button2.AutoSize = True
+        Button2.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button2.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button2.Location = New Point(245, 407)
+        Button2.MinimumSize = New Size(64, 56)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(64, 56)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button3.AutoSize = True
+        Button3.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button3.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button3.Location = New Point(380, 407)
+        Button3.MinimumSize = New Size(64, 56)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(64, 56)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button4.AutoSize = True
+        Button4.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button4.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button4.Location = New Point(114, 320)
+        Button4.MinimumSize = New Size(64, 56)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(64, 56)
+        Button4.TabIndex = 4
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button5.AutoSize = True
+        Button5.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button5.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button5.Location = New Point(245, 320)
+        Button5.MinimumSize = New Size(64, 56)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(64, 56)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button6.AutoSize = True
+        Button6.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button6.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button6.Location = New Point(380, 320)
+        Button6.MinimumSize = New Size(64, 56)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(64, 56)
+        Button6.TabIndex = 6
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button7.AutoSize = True
+        Button7.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button7.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button7.Location = New Point(114, 238)
+        Button7.MinimumSize = New Size(64, 56)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(64, 56)
+        Button7.TabIndex = 7
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button8.AutoSize = True
+        Button8.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button8.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button8.Location = New Point(245, 238)
+        Button8.MinimumSize = New Size(64, 56)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(64, 56)
+        Button8.TabIndex = 8
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button9.AutoSize = True
+        Button9.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button9.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button9.Location = New Point(380, 238)
+        Button9.MinimumSize = New Size(64, 56)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(64, 56)
+        Button9.TabIndex = 9
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button10.AutoSize = True
+        Button10.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button10.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Button10.Location = New Point(114, 492)
+        Button10.MinimumSize = New Size(200, 56)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(200, 56)
+        Button10.TabIndex = 10
+        Button10.Text = "0"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Point
+        ' 
+        Point.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Point.AutoSize = True
+        Point.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Point.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Point.Location = New Point(380, 492)
+        Point.MinimumSize = New Size(64, 56)
+        Point.Name = "Point"
+        Point.Size = New Size(64, 56)
+        Point.TabIndex = 11
+        Point.Text = "."
+        Point.UseVisualStyleBackColor = True
+        ' 
+        ' EqualBtn
+        ' 
+        EqualBtn.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        EqualBtn.AutoSize = True
+        EqualBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        EqualBtn.BackColor = Color.Purple
+        EqualBtn.FlatStyle = FlatStyle.Popup
+        EqualBtn.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        EqualBtn.Location = New Point(512, 492)
+        EqualBtn.MinimumSize = New Size(64, 56)
+        EqualBtn.Name = "EqualBtn"
+        EqualBtn.Size = New Size(64, 56)
+        EqualBtn.TabIndex = 12
+        EqualBtn.Text = "="
+        EqualBtn.UseVisualStyleBackColor = False
+        ' 
+        ' PlusBtn
+        ' 
+        PlusBtn.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        PlusBtn.AutoSize = True
+        PlusBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        PlusBtn.BackColor = Color.DeepPink
+        PlusBtn.FlatStyle = FlatStyle.Popup
+        PlusBtn.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        PlusBtn.Location = New Point(512, 407)
+        PlusBtn.MinimumSize = New Size(64, 56)
+        PlusBtn.Name = "PlusBtn"
+        PlusBtn.Size = New Size(64, 56)
+        PlusBtn.TabIndex = 13
+        PlusBtn.Text = "+"
+        PlusBtn.UseVisualStyleBackColor = False
+        ' 
+        ' MinusBtn
+        ' 
+        MinusBtn.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        MinusBtn.AutoSize = True
+        MinusBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        MinusBtn.BackColor = Color.DeepPink
+        MinusBtn.FlatStyle = FlatStyle.Popup
+        MinusBtn.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        MinusBtn.Location = New Point(512, 320)
+        MinusBtn.MinimumSize = New Size(64, 56)
+        MinusBtn.Name = "MinusBtn"
+        MinusBtn.Size = New Size(64, 56)
+        MinusBtn.TabIndex = 14
+        MinusBtn.Text = "-"
+        MinusBtn.UseVisualStyleBackColor = False
+        ' 
+        ' MulBtn
+        ' 
+        MulBtn.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        MulBtn.AutoSize = True
+        MulBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        MulBtn.BackColor = Color.DeepPink
+        MulBtn.FlatStyle = FlatStyle.Popup
+        MulBtn.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        MulBtn.Location = New Point(512, 238)
+        MulBtn.MinimumSize = New Size(64, 56)
+        MulBtn.Name = "MulBtn"
+        MulBtn.Size = New Size(64, 56)
+        MulBtn.TabIndex = 15
+        MulBtn.Text = "*"
+        MulBtn.UseVisualStyleBackColor = False
+        ' 
+        ' DivBtn
+        ' 
+        DivBtn.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        DivBtn.AutoSize = True
+        DivBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        DivBtn.BackColor = Color.DeepPink
+        DivBtn.FlatStyle = FlatStyle.Popup
+        DivBtn.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        DivBtn.Location = New Point(512, 157)
+        DivBtn.MinimumSize = New Size(64, 56)
+        DivBtn.Name = "DivBtn"
+        DivBtn.Size = New Size(64, 56)
+        DivBtn.TabIndex = 16
+        DivBtn.Text = "/"
+        DivBtn.UseVisualStyleBackColor = False
+        ' 
+        ' Back
+        ' 
+        Back.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Back.AutoSize = True
+        Back.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Back.Font = New Font("A-OTF Jun Pro 34", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Back.Location = New Point(358, 157)
+        Back.MinimumSize = New Size(64, 56)
+        Back.Name = "Back"
+        Back.Size = New Size(86, 56)
+        Back.TabIndex = 17
+        Back.Text = "Back"
+        Back.UseVisualStyleBackColor = True
+        ' 
+        ' Clear
+        ' 
+        Clear.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Clear.AutoSize = True
+        Clear.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Clear.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold)
+        Clear.Location = New Point(245, 160)
+        Clear.MinimumSize = New Size(64, 56)
+        Clear.Name = "Clear"
+        Clear.Size = New Size(64, 56)
+        Clear.TabIndex = 18
+        Clear.Text = "C"
+        Clear.UseVisualStyleBackColor = True
+        ' 
+        ' Clear_All
+        ' 
+        Clear_All.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Clear_All.AutoSize = True
+        Clear_All.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Clear_All.Font = New Font("A-OTF Jun Pro 34", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Clear_All.Location = New Point(114, 160)
+        Clear_All.MaximumSize = New Size(84, 76)
+        Clear_All.MinimumSize = New Size(64, 56)
+        Clear_All.Name = "Clear_All"
+        Clear_All.Size = New Size(64, 56)
+        Clear_All.TabIndex = 19
+        Clear_All.Text = "CE"
+        Clear_All.UseVisualStyleBackColor = True
+        ' 
+        ' expression
+        ' 
+        expression.AutoSize = True
+        expression.Font = New Font("Segoe UI", 13F)
+        expression.ForeColor = Color.White
+        expression.Location = New Point(475, 37)
+        expression.Name = "expression"
+        expression.Size = New Size(0, 25)
+        expression.TabIndex = 20
+        expression.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(64), CByte(0), CByte(64))
+        ClientSize = New Size(660, 622)
+        Controls.Add(expression)
+        Controls.Add(Clear_All)
+        Controls.Add(Clear)
+        Controls.Add(Back)
+        Controls.Add(DivBtn)
+        Controls.Add(MulBtn)
+        Controls.Add(MinusBtn)
+        Controls.Add(PlusBtn)
+        Controls.Add(EqualBtn)
+        Controls.Add(Point)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimumSize = New Size(676, 638)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Point As Button
+    Friend WithEvents EqualBtn As Button
+    Friend WithEvents PlusBtn As Button
+    Friend WithEvents MinusBtn As Button
+    Friend WithEvents MulBtn As Button
+    Friend WithEvents DivBtn As Button
+    Friend WithEvents Back As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Clear_All As Button
+    Friend WithEvents expression As Label
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,208 @@
+﻿
+Public Class Form1
+    Dim myList As New List(Of String)()
+    Dim temp, str, ans As String
+
+    Private Sub Print()
+        If ans IsNot Nothing Then
+            expression.Text = ""
+            TextBox1.Text = ""
+            ans = Nothing
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button1.Text
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button2.Text
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button3.Text
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button4.Text
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button5.Text
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button6.Text
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button7.Text
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button8.Text
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button9.Text
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Button10.Text
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Point.Click
+        Print()
+        TextBox1.Text = TextBox1.Text & Point.Text
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles PlusBtn.Click
+        If myList.Count > 0 Or TextBox1.Text.Length > 0 Then
+            str = ""
+            expression.Text = expression.Text & PlusBtn.Text
+            myList.Add(TextBox1.Text)
+            If temp IsNot Nothing Then
+                myList.Add(temp)
+                temp = Nothing
+            End If
+            temp = PlusBtn.Text
+            TextBox1.Clear()
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles MinusBtn.Click
+        If myList.Count > 0 Or TextBox1.Text.Length > 0 Then
+
+            str = ""
+            expression.Text = expression.Text & MinusBtn.Text
+            myList.Add(TextBox1.Text)
+            If temp IsNot Nothing Then
+                myList.Add(temp)
+                temp = Nothing
+            End If
+            temp = MinusBtn.Text
+            TextBox1.Clear()
+
+        End If
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles MulBtn.Click
+        If myList.Count > 0 Or TextBox1.Text.Length > 0 Then
+
+            str = ""
+            expression.Text = expression.Text & MulBtn.Text
+            myList.Add(TextBox1.Text)
+            If temp IsNot Nothing Then
+                myList.Add(temp)
+                temp = Nothing
+            End If
+            temp = MulBtn.Text
+            TextBox1.Clear()
+        End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles DivBtn.Click
+        If myList.Count > 0 Or TextBox1.Text.Length > 0 Then
+
+            str = ""
+            expression.Text = expression.Text & DivBtn.Text
+            myList.Add(TextBox1.Text)
+            If temp IsNot Nothing Then
+                myList.Add(temp)
+                temp = Nothing
+            End If
+            temp = DivBtn.Text
+            TextBox1.Clear()
+
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles EqualBtn.Click
+        str = ""
+        expression.Text = expression.Text & EqualBtn.Text
+        myList.Add(TextBox1.Text)
+        If temp IsNot Nothing Then
+            myList.Add(temp)
+            temp = Nothing
+        End If
+
+        TextBox1.Text = EvalRPN(myList.ToArray()).ToString() & " "
+        ans = " "
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Back.Click
+        Print()
+        If TextBox1.Text.Count > 0 Then
+            TextBox1.Text = TextBox1.Text.Substring(0, TextBox1.Text.Length - 1)
+            If expression.Text.Length > 1 Then
+                expression.Text = expression.Text.Substring(0, expression.Text.Length - 2)
+            Else
+                expression.Text = ""
+            End If
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        expression.Text = expression.Text.Replace(TextBox1.Text, "")
+        Print()
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Clear_All.Click
+        TextBox1.Clear()
+        expression.Text = ""
+        If myList.Count < 0 Then
+            myList.Clear()
+        End If
+    End Sub
+
+    Public Function EvalRPN(tokens As String()) As Double
+        Dim operators As New HashSet(Of String) From {"+", "-", "*", "/"}
+        Dim stack As New Stack(Of String)()
+
+        For Each token As String In tokens
+            If operators.Contains(token) Then
+                Dim var2 As Double = Double.Parse(stack.Pop())
+                Dim var1 As Double = Double.Parse(stack.Pop())
+                Dim ans As Double
+
+                Select Case token
+                    Case "+"
+                        ans = var1 + var2
+                    Case "-"
+                        ans = var1 - var2
+                    Case "*"
+                        ans = var1 * var2
+                    Case "/"
+                        ans = var1 / var2
+                    Case Else
+                        Throw New ArgumentException("Invalid operator: " & token)
+                End Select
+                stack.Push(ans.ToString())
+            Else
+                stack.Push(token)
+            End If
+        Next
+
+        Return Double.Parse(stack.Pop())
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        str = sender.Text
+        If TextBox1.Text.Length = 1 Then
+            expression.Text = expression.Text & str
+        ElseIf TextBox1.Text.Length > 1 Then
+            expression.Text = expression.Text & str.ElementAt(str.Length - 1)
+        End If
+    End Sub
+End Class
